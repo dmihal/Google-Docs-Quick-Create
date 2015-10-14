860800d0701c030a58d7ceeb7ee779aa45b3f810
David Mihal <eastsideski@gmail.com>
2015-10-14 00:03:16 -0400

    Update URLs in popup for apps domains

---
+++ popup.js
@@ -0,0 +1,18 @@
+(function(){
+  var apps_urls = {
+    doc: "https://docs.google.com/a/??/document/create",
+    sheet: "https://docs.google.com/a/??/spreadsheet/ccc?new",
+    prez: "https://docs.google.com/a/??/presentation/create",
+    draw: "https://docs.google.com/a/??/drawings/create",
+    form: "https://docs.google.com/a/??/forms/create"
+  };
+
+  chrome.storage.sync.get('apps_domain', function(values){
+    if (values.apps_domain){
+      for (var link_id in apps_urls){
+        var url = apps_urls[link_id].replace("??", values.apps_domain);
+        document.getElementById(link_id).firstChild.href = url;
+      }
+    }
+  });
+})();
