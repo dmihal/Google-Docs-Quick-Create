d6a84a63e8eb9bfc5dd7a5e9a7787db2dff37ce4
David Mihal <eastsideski@gmail.com>
2015-11-22 13:14:53 -0700

    Fix options page functionality with MDL.

--- options.js
+++ options.js
@@ -1,23 +1,27 @@
-(function(){
-  var radios = document.querySelectorAll('input[name=acct_type]');
+window.addEventListener('load', function(){
+  var radios = document.querySelectorAll('.mdl-js-radio');
   var domain_field = document.querySelector('#txt_domain');
   var apps_radio = null;
 
+  MaterialRadio.prototype.isChecked = function(){
+    return this.btnElement_.checked
+  };
+
   // Add event listeners to radio buttons
   for (var i = 0; i < radios.length; i++) {
-    var radio = radios[i];
+    var radio = radios[i].MaterialRadio.btnElement_;
     radio.addEventListener('change', function(e){
-      domain_field.disabled = !apps_radio.checked;
+      domain_field.disabled = !apps_radio.isChecked();
       form_changed_event();
     });
     if (radio.value == 'apps'){
-      apps_radio = radio;
+      apps_radio = radios[i].MaterialRadio;
     }
   };
 
   // Set up event listener to store domain
   var form_changed_event = function(){
-    var isApps = apps_radio.checked && domain_field.validity.valid;
+    var isApps = apps_radio.isChecked() && domain_field.validity.valid;
     var domain = isApps ? domain_field.value : null;
     chrome.storage.sync.set({'apps_domain': domain}, function() {
       // Settings saved
@@ -28,9 +32,10 @@
   // Prefill form values
   chrome.storage.sync.get('apps_domain', function(values){
     if (values.apps_domain){
-      apps_radio.checked = true;
+      apps_radio.check();
+      apps_radio.onChange_();
       domain_field.disabled = false;
       domain_field.value = values.apps_domain;
     }
   });
-})();
+});
