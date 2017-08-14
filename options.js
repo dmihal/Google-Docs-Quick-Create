af9b88cd8453885ecfb528e0b375fb7e1efae516
austencm <austencm@gmail.com>
2017-08-14 12:13:45 -0700

    Add ability to switch user accounts on the fly

--- options.js
+++ options.js
@@ -1,35 +1,52 @@
-let radios = document.querySelectorAll('input[name=acct_type]'),
-    gsuite_radio = document.querySelector('input[name=acct_type][value=apps]'),
-    gsuite_domain_field = document.querySelector('#gsuite_domain')
-
-/**
- * Saves G Suite domain in Chrome storage,
- * or sets to null if G Suite is disabled
- */
-function set_gsuite_domain() {
-  let is_gsuite = gsuite_radio.checked && gsuite_domain_field.validity.valid
-  let domain = is_gsuite ? gsuite_domain_field.value : null
-
-  chrome.storage.sync.set({'apps_domain': domain})
+// Option defaults
+const defaults = {
+  apps_domain: '',
+  acct_count: 1,
+  acct_selected: 1
 }
-gsuite_domain_field.addEventListener('change', set_gsuite_domain)
+
+var options,
+    inputs = document.querySelectorAll('input'),
+    gsuite_radio = document.querySelector('input[name=acct_type][value=apps]'),
+    gsuite_domain_field = document.querySelector('#gsuite_domain'),
+    acct_count_field = document.querySelector('#acct_count')
 
 
-// Add event listener to radio buttons
-radios.forEach((radio) => {
-  radio.addEventListener('change', () => {
-    // Toggle domain field
-    gsuite_domain_field.disabled = !gsuite_radio.checked
+// Option defaults
+function update_options() {
+  // Did the user specify a G Suite domain?
+  let is_gsuite = gsuite_radio.checked && gsuite_domain_field.validity.valid
+  options.apps_domain = is_gsuite ? gsuite_domain_field.value : null
+  // If the accout count is invalid, set it to 1
+  options.acct_count = acct_count_field.validity.valid ? acct_count_field.value : 1
 
-    set_gsuite_domain()
-  })
-})
+  // Reset the account selected if it's greater than the new account count
+  if (options.acct_selected >= options.acct_count)
+    options.acct_selected = 0
+
+  // Save options
+  chrome.storage.sync.set(options)
+}
 
 // Load options state
-chrome.storage.sync.get('apps_domain', (values) => {
-  if (values.apps_domain) {
+chrome.storage.sync.get(defaults, (items) => {
+  options = items
+  acct_count_field.value = options.acct_count
+
+  if (options.apps_domain) {
     gsuite_radio.checked = true
     gsuite_domain_field.disabled = false
-    gsuite_domain_field.value = values.apps_domain
+    gsuite_domain_field.value = options.apps_domain
   }
+
+  // Update options when an input is changed
+  inputs.forEach((input) => {
+    input.addEventListener('change', (e) => {
+      // Enable domain field if the G Suite option is selected
+      if (e.target.type === 'radio')
+        gsuite_domain_field.disabled = !gsuite_radio.checked
+
+      update_options()
+    })
+  })
 })
--- popup.js
+++ popup.js
@@ -1,24 +1,102 @@
-(function(){
-  var apps_urls = {
-    doc: "https://docs.google.com/a/??/document/create",
-    sheet: "https://docs.google.com/a/??/spreadsheet/ccc?new",
-    prez: "https://docs.google.com/a/??/presentation/create",
-    draw: "https://docs.google.com/a/??/drawings/create",
-    form: "https://docs.google.com/a/??/forms/create"
-  };
+// Option defaults
+const defaults = {
+  apps_domain: '',
+  acct_count: 1,
+  acct_selected: 1
+}
 
-  chrome.storage.sync.get('apps_domain', function(values){
-    if (values.apps_domain){
-      for (var link_id in apps_urls){
-        var url = apps_urls[link_id].replace("??", values.apps_domain);
-        document.getElementById(link_id).firstElementChild.href = url;
-      }
-      document.getElementById('status').innerText = `Using the ${values.apps_domain} domain`;
-    }
-  });
+var options,
+    // These keys should match the i18n keys and icon filenames
+    doc_types = ['document', 'spreadsheet', 'presentation', 'drawing', 'form'],
+    // We'll build the URL with these components
+    url_comps = {
+      base: 'https://docs.google.com',
+      gsuite: '/a/??',
+      acct: '/u/??',
+      suffix: '/create'
+    },
+    links_wrap = document.querySelector('#links'),
+    acct_select = document.querySelector('#acct_select')
 
-  for (var link_id in apps_urls) {
-    var localLabel = chrome.i18n.getMessage("new_" + link_id);
-    document.querySelector(`#${link_id} .label`).innerText = localLabel;
+
+/**
+ * Rebuilds and replaces the link list in the DOM
+ */
+function update_links() {
+  // Replace placholders with user config
+  let url_gsuite = options.apps_domain ? url_comps.gsuite.replace('??', options.apps_domain) : '',
+      url_acct = url_comps.acct.replace('??', options.acct_selected),
+      links = ''
+
+  // Build the link list
+  doc_types.forEach((doc_type) => {
+    let url,
+        url_doc_type = doc_type
+    // A few of the URLs require the doc_type to plural because Google is weird
+    if ( 'spreadsheet drawing form'.includes(url_doc_type) )
+      url_doc_type += 's'
+    // Build URL
+    url = `${url_comps.base}${url_gsuite}/${url_doc_type}${url_acct}${url_comps.suffix}`
+
+    // I18n for link labels
+    let label = chrome.i18n.getMessage('new_' + doc_type)
+
+    // Create link UI
+    links += `
+      <li>
+        <a href="${url}" target="_blank">
+          <img src="${doc_type}-32.png" width="32" height="32">
+          <label>${label}</label>
+        </a>
+      </li>
+    `
+  })
+  links_wrap.innerHTML = links
+}
+
+/**
+ * Updates UI and saves currently selected account index
+ * Triggered by clicking a numbered button
+ */
+function select_acct() {
+  this.parentElement.querySelector('.active').classList.remove('active')
+  this.classList.add('active')
+
+  options.acct_selected = parseInt(this.textContent) - 1
+  update_links()
+  chrome.storage.sync.set({ acct_selected: options.acct_selected })
+}
+
+/**
+ * Adds account selection UI and related event listeners
+ */
+function enable_acct_select() {
+  // Create account select UI
+  let accts_html = ''
+  for (let i = 0; i < options.acct_count; i++) {
+    let active = (i === options.acct_selected ? 'active' : '')
+    accts_html += `<button class="${active}">${i + 1}</button>`
   }
-})();
+  acct_select.innerHTML = accts_html
+
+  // Select account when a button is clicked
+  document.querySelectorAll('#acct_select button').forEach((btn) => {
+    btn.addEventListener( 'click', (e) => select_acct.bind(e.target)() )
+  })
+}
+
+// Fetch user configuration
+chrome.storage.sync.get(defaults, (items) => {
+  // Save options globally to use elsewhere
+  options = items
+
+  // Create initial link UI
+  update_links()
+
+  // If the user has more than one account, enable account selection
+  if (options.acct_count > 1)
+    enable_acct_select()
+  // If the user specified a G Suite domain, show it in the status box
+  if (options.apps_domain)
+    document.querySelector('#status').innerText = `G Suite: ${options.apps_domain}`;
+})
